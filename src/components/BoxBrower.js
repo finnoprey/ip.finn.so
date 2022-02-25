48c3794b0afe643e1523846519f5f58891b814d8
finnoprey <finn@oprey.co>
2022-02-26 10:51:03 +1100

    Added main components and styling

---
+++ src/components/BoxBrower.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './BoxBrowser.css'
+
+export default function BoxBrowser() {
+  return (
+    <div className='box-browser'>
+      <h1>Browser Information</h1>
+      <p1><i>Coming Soon...</i></p1>
+    </div>
+  )
+}
---
+++ src/components/BoxTitle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './BoxTitle.css'
+
+export default function BoxTitle() {
+  return (
+    <div className='box-primary'>
+      <h1>Public Address</h1>
+			<p1 id="ip-placeholder">***.***.**.**</p1>
+    </div>
+  )
+}
