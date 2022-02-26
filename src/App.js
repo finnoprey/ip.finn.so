d0bab4a3b88499f2c57dd8197c8792d575f0ea16
finnoprey <finn@oprey.co>
2022-02-26 11:39:43 +1100

    Added api functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import BoxBrowser from './components/BoxBrower';
 import BoxLookup from './components/BoxLookup';
 import BoxTitle from './components/BoxTitle';
 
 function App() {
-  // make api call here
+  const url = 'https://api.oprey.co/'
+  const placeholder = {
+    ip: '***.***.**.**',
+    status: 'fetching',
+    country: '*********',
+    region: '*********',
+    city: '*********',
+    isp: '*********'
+  }
+  
+  const [ipData, setIpData] = useState(placeholder)
+
+  useEffect(() => {
+    fetch (url + 'ip')
+    .then(res => res.json())
+    .then(json => {
+      setIpData(json)
+    })
+  }, [ url ])
 
   return (
     <div className='container'>
-      <BoxTitle />
-      <BoxLookup />
+      <BoxTitle ipData={ipData}/>
+      <BoxLookup ipData={ipData}/>
       <BoxBrowser />
     </div>
   );
--- src/components/BoxLookup.js
+++ src/components/BoxLookup.js
@@ -1,24 +1,14 @@
 import React from 'react'
 import './BoxLookup.css'
 
-export default function BoxLookup() {
+export default function BoxLookup(props) {
   return (
-    <div class='box-lookup'>
+    <div className='box-lookup'>
       <h1>Address Lookup</h1>
-      <p1><strong>Country </strong></p1>
-      <p1 id='country-placeholder'>*********</p1>
-      
-      <br />
-      <p1><strong>Region </strong></p1>
-      <p1 id='region-placeholder'>*********</p1>
-      
-      <br />
-      <p1><strong>City </strong></p1>
-      <p1 id='city-placeholder'>*********</p1>
-
-      <br />
-      <p1><strong>ISP </strong></p1>
-      <p1 id='isp-placeholder'>*********</p1>
+      <p><strong>Country </strong> { props.ipData.country }</p>
+      <p><strong>Region </strong> { props.ipData.region }</p>
+      <p><strong>City </strong> { props.ipData.city }</p>
+      <p><strong>ISP </strong> { props.ipData.isp }</p>
     </div>
   )
 }
